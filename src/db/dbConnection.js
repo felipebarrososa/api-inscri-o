8c8337c768068fe2fee9ba9445ce055df07bfa76
Matheus2ouza <matifurtado0@gmail.com>
2024-10-21 16:19:49 -0300

    troca das variaveis de ambiete

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -3,11 +3,10 @@ const { Pool } = require('pg');
 
 // Dados para conexão ao banco de dados
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DATABASE,
 });
 
 let isConnected = false; // Variável de controle
