17d8131e0d1fc94e41f8818e0d6e9f2344a6885f
Matheus2ouza <matifurtado0@gmail.com>
2024-10-22 03:58:01 -0300

    apagado todas as imagens

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -1,10 +1,12 @@
-import pg from 'pg';
-
-const { Pool } = pg;
+const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
-})
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+});
 
 let isConnected = false; // Variável de controle
 
