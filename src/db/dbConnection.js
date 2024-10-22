2a1aca1be6e8564abe850d579d4d7d5d7d9aa6df
Matheus2ouza <matifurtado0@gmail.com>
2024-10-22 04:01:11 -0300

    mudança no db

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -1,12 +1,10 @@
-const { Pool } = require('pg');
+import pg from 'pg';
+
+const { Pool } = pg;
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-});
+  connectionString: process.env.POSTGRES_URL,
+})
 
 let isConnected = false; // Variável de controle
 
