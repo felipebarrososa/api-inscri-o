7dc3e8f8110ebcf159fd50a6e06f700ffa0e4f8e
felipebarrosoa <fsbarroso0890@gmail.com>
2024-10-29 10:32:57 -0300

    corrigindo o dashboard

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const logger = require('../../utils/logger');
 const { pool } = require('../db/dbConnection');
 
 // Rota para obter dados do dashboard
