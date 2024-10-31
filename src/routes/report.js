475665a0ff18d3ffd4c75ff2215554549b55885b
felipebarrosoa <fsbarroso0890@gmail.com>
2024-10-31 12:54:27 -0300

    corrigindo o report.js

--- src/routes/report.js
+++ src/routes/report.js
@@ -2,24 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { pool } = require('../db/dbConnection');
 
-// Rota para obter todas as localidades
-router.get('/', async (req, res) => {
-    try {
-        const locations = await pool.query('SELECT * FROM localidades');
-        const result = locations.rows;
-
-        if (result.length === 0) { // Verifica se não há localidades
-            console.warn('Consulta de localidade feita, mas não teve nenhum resultado.');
-            return res.status(404).json({ message: 'Nenhuma localidade encontrada.' });
-        } else {
-            console.info('Consulta de localidade feita com sucesso.');
-            return res.status(200).json(result); // Código 200 para OK
-        }
-    } catch (err) {
-        console.error(`Erro ao buscar localidade: ${err}`);
-        return res.status(500).json({ error: 'Erro ao buscar localidade.' });
-    }
-});
 
 // Rota para obter o relatório com base no ID da localidade
 router.get('/:localidadeId?', async (req, res) => {
